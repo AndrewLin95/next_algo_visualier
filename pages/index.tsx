9a60aab19412b840eefd7367e9b71b032ad9fe40
AndrewLin95 <andrew.lin88@hotmail.com>
2023-02-04 13:54:34 -0500

    updated imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Algo Visualizer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -80,7 +80,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
