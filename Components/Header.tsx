a6b359278d4219e60df66236987047405a41337c
AndrewLin95 <andrew.lin88@hotmail.com>
2023-02-04 19:37:34 -0500

    updated header with data from the utils pagedata folder

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link';
+import { pagesData } from '../utils/pagesData';
+
+const pageData = pagesData();
 
 const Header = () => {
   return (
-    <div className="w-full h-12 flex flex-row justify-between items-center px-5 border-2 border-red-800">
-      <div>Algo Visualizer</div>
+    <div className="w-full h-14 flex flex-row justify-between items-center px-5 border-2 border-red-800">
+      <div>
+        <Link href="/">Algo Visualizer</Link>
+      </div>
       <div className="flex flex-row gap-3">
-        <Link href="/sort">Sort</Link>
-        <Link href="/search">Search</Link>
+        {Object.entries(pageData).map(([key, value]) => (
+          <Link href={value.link} key={key}>
+            <div>{value.name}</div>
+          </Link>
+        ))}
       </div>
     </div>
   );
